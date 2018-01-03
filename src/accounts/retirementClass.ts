74c7f03d8e6fb400f7ca4a5eef79af0957b89fb1
jared <jared.jacobson.2000@gmail.com>
2018-01-02 18:19:58 -0700

    created RetirementAccount class

---
+++ src/accounts/retirementClass.ts
@@ -0,0 +1,28 @@
+import { Account } from "./accountClass";
+import { TransactionOrigin } from "../transaction/transactionOriginEnum";
+import { Transaction } from "../transaction/transactionInterface";
+import { displayClassNameWithPurpose } from "../decorators";
+import { TransactionClass } from "../transaction/transactionClass";
+
+@displayClassNameWithPurpose('An account that collects good interest, but is not easy to withdraw from.')
+export class RetirementAccount extends Account {
+    constructor(name: string, birthDate: Date) {
+        super(name, birthDate);
+        this.balance = 100000;
+        this.interest = .03;
+    }
+    withdrawMoney(amount: number, description: string, transactionType: TransactionOrigin): Transaction {
+        // this.currentTransaction = new TransactionClass(amount, transactionType);
+        // if(transactionType === TransactionOrigin.branch && this.month[transactionType] < 1000) {
+        //     this.balanceCheck(amount, transactionType);
+        // }
+        // else if(this.month[transactionType] < 6) {
+        //     this.balanceCheck(amount, transactionType);
+        // }
+        // else {
+        //     this.failWithdraw('You have made to many withdrawls via the ' + TransactionOrigin[this.currentTransaction.type]);
+        // }
+        // return this.completeTransaction();
+        throw new Error('method not implemented');
+    }
+}
